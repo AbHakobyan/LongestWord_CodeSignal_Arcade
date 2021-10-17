795cfc2bb93a03520f22257e3a6f1da608ccb141
Albert Hakobyan <abhakobyan24@gmail.com>
2021-10-17 14:48:01 +0400

    Добавьте файлы проекта.

---
+++ LongestWord_CodeSignal_Arcade/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestWord_CodeSignal_Arcade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "To be or not to be";
+            string st = longestWord(s);
+            Console.WriteLine(st);
+        }
+
+        static string longestWord(string text)
+        {
+            return Regex.Split(text, @"[^a-zA-Z]+").OrderByDescending(_ => _.Length).First();
+        }
+    }
+}
